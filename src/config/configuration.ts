87fa0fcb8e699d6e690c458a88a75e401391878d
Md-AbdullahAl-Noman <abdullahnoman1414@gmail.com>
2025-09-02 17:34:29 +0600

    Update database provider to PostgreSQL and adjust connection settings

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   port: parseInt(process.env.PORT || '8000', 10),
   database_url: {
-    url: process.env.DB_CONNECTION_STRING,
+    url: process.env.DATABASE_URL,
   },
   mailgun: {
     username: process.env.MAILGUN_USERNAME || '',
